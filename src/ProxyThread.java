95cdb90a3c57b8875ecccc3e35423abe92909d00
Close Studios <closestudios@Closes-MacBook-Pro.local>
2015-10-04 20:28:13 -0500

    more

--- src/ProxyThread.java
+++ src/ProxyThread.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.HashMap;
@@ -29,32 +30,40 @@ public class ProxyThread extends Thread{
             OutputStream outToClient = socket.getOutputStream();
             InputStream inFromClient = socket.getInputStream();
 
+
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-            int nRead;
             byte[] data = new byte[1024];
-
-            System.out.println("Reading Data: " + inFromClient.read(data, 0, data.length));
-            System.out.println("Reading Data2: " + inFromClient.read(data, 0, data.length));
-            System.out.println("Reading Data3: " + inFromClient.read(data, 0, data.length));
-
+            boolean dataOver = false;
+            while(!dataOver) {
+                int length = inFromClient.read(data, 0, data.length);
+                if(length != -1) {
+                    buffer.write(data, 0, length);
+                }
+                if(length == -1 || inFromClient.available() == 0) {
+                    dataOver = true;
+                }
+            }
 
             buffer.flush();
             byte[] requestFromClient = buffer.toByteArray();
-            System.out.println("Read Data: " + requestFromClient.length);
-
-            /*
-
+            String requestFromClientString = new String(requestFromClient);
+            System.out.println("Request Data From Client: " + requestFromClientString);
+            String[] lines = requestFromClientString.split(System.getProperty("line.separator"));
 
             // Get Request
-            String inputLine = "";
             Request request = new Request();
-            while ((inputLine = in.readLine()) != null) {
+            for(int i = 0;i < lines.length;i++) {
+
+                String inputLine = lines[i];
                 System.out.println("Read: " + inputLine);
                 StringTokenizer tok = new StringTokenizer(inputLine);
 
-                if(tok.nextToken().equals("GET")) {
-                    request.setURL(tok.nextToken());
-                    System.out.println("Found GET");
+                if(tok.hasMoreTokens()) {
+                    String token = tok.nextToken();
+                    if (token != null && token.equals("GET")) {
+                        request.setURL(tok.nextToken());
+                        System.out.println("Found GET");
+                    }
                 }
 
             }
@@ -62,7 +71,11 @@ public class ProxyThread extends Thread{
             if(request.isValidRequest()) {
                 // Create socket to server
 
-                Socket serverSocket = new Socket(request.getURL,80);
+                InetAddress address = InetAddress.getByName(request.getURL);
+                String ipAddress = address.getHostAddress();
+                System.out.println("IP Address: " + ipAddress);
+
+                Socket serverSocket = new Socket(ipAddress,80);
 
                 OutputStream outToServer = serverSocket.getOutputStream();
                 InputStream inFromServer = serverSocket.getInputStream();
@@ -70,9 +83,25 @@ public class ProxyThread extends Thread{
                 outToServer.write(requestFromClient);
 
 
-            }
+                ByteArrayOutputStream bufferFromServer = new ByteArrayOutputStream();
+                byte[] dataFrom = new byte[1024];
+                boolean dataFromOver = false;
+                while(!dataFromOver) {
+                    int length = inFromServer.read(dataFrom, 0, dataFrom.length);
+                    if(length != -1) {
+                        bufferFromServer.write(dataFrom, 0, length);
+                    }
+                    if(length == -1 || inFromServer.available() == 0) {
+                        dataFromOver = true;
+                    }
+                }
 
-*/
+                bufferFromServer.flush();
+                byte[] requestFromServer = bufferFromServer.toByteArray();
+                System.out.println("Request Data From Server: " + requestFromServer.length);
+
+
+            }
 
 
             if (socket != null) {
