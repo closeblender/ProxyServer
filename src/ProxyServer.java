2607aa14f94f69f5d13eabaa98be4e7f9bc1f79a
Close Studios <closestudios@Closes-MacBook-Pro.local>
2015-10-04 19:21:30 -0500

    more

--- src/ProxyServer.java
+++ src/ProxyServer.java
@@ -9,9 +9,9 @@ public class ProxyServer {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        ServerSocket server;
+        ServerSocket server = null;
         boolean serverRunning = true;
 
         int port = 9090; // Default
