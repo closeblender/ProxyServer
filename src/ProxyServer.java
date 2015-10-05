7f1a72210722b7ded6b96ae5ea86b8caca51332a
Close Studios <closestudios@Closes-MacBook-Pro.local>
2015-10-04 19:20:21 -0500

    more

--- src/ProxyServer.java
+++ src/ProxyServer.java
@@ -1,4 +1,6 @@
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by closestudios on 10/4/15.
@@ -12,7 +14,26 @@ public class ProxyServer {
         ServerSocket server;
         boolean serverRunning = true;
 
+        int port = 9090; // Default
+        try { // Try to get args
+            port = Integer.parseInt(args[0]);
+        } catch (Exception e) {
 
+        }
+
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Started on: " + port);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + args[0]);
+            System.exit(-1);
+        }
+
+        while (serverRunning) {
+            Socket acceptedSocket = server.accept();
+            new ProxyThread(acceptedSocket, acceptedSocket.getRemoteSocketAddress()).start();
+        }
+        server.close();
 
     }
 
--- src/ProxyThread.java
+++ src/ProxyThread.java
@@ -1,8 +1,26 @@
+import java.net.Socket;
+import java.net.SocketAddress;
+
 /**
  * Created by closestudios on 10/4/15.
  */
-public class ProxyThread {
+public class ProxyThread extends Thread{
 
+    private Socket socket = null;
+    private SocketAddress clientAddress = null;
+
+    public ProxyThread(Socket socket, SocketAddress address) {
+        super("ProxyThread");
+        this.socket = socket;
+        this.clientAddress = address;
+    }
+
+
+    public void run() {
+
+
+
+    }
 
 
 }
