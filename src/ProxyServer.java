6602f8a346a2c9bce01c6e7eb641c15ef2ff0a83
Close Studios <closestudios@Closes-MacBook-Pro.local>
2015-10-04 19:16:43 -0500

    stuff

---
+++ src/ProxyServer.java
@@ -0,0 +1,19 @@
+import java.net.ServerSocket;
+
+/**
+ * Created by closestudios on 10/4/15.
+ */
+public class ProxyServer {
+
+
+
+    public static void main(String[] args) {
+
+        ServerSocket server;
+        boolean serverRunning = true;
+
+
+
+    }
+
+}
---
+++ src/ProxyThread.java
@@ -0,0 +1,8 @@
+/**
+ * Created by closestudios on 10/4/15.
+ */
+public class ProxyThread {
+
+
+
+}
