6683bcf3816219373adb5b0d1e75345259bd4de8
Close Studios <closestudios@Closes-MacBook-Pro.local>
2015-10-04 19:38:21 -0500

    bytes

--- src/ProxyThread.java
+++ src/ProxyThread.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.net.Socket;
 import java.net.SocketAddress;
 
@@ -18,7 +19,44 @@ public class ProxyThread extends Thread{
 
     public void run() {
 
+        try {
+            // Print out Client
+            System.out.println("Client Connected: " + clientAddress.toString());
 
+            // Set up data streams
+            OutputStream outToClient = socket.getOutputStream();
+            InputStream inFromClient = socket.getInputStream();
+
+            // Get all the bytes from the client
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+            int nRead;
+            byte[] data = new byte[16384];
+
+            while ((nRead = inFromClient.read(data, 0, data.length)) != -1) {
+                System.out.println("Reading");
+                buffer.write(data, 0, nRead);
+            }
+
+            buffer.flush();
+
+            byte[] inputFromClient = buffer.toByteArray();
+
+            System.out.println("In From Client Size: " + inputFromClient.length);
+
+            String requestFromClient = new String(inputFromClient);
+
+            System.out.println("Request: " + requestFromClient);
+
+
+            if (socket != null) {
+                socket.close();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
